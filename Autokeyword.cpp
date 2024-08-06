120123f73678bdd634098d9f940f80b7cb3a03b3
Pushpraj Singh Chouhan <113308159+PushprajSinghChouhan@users.noreply.github.com>
2024-08-06 16:32:42 +0530

    Update Autokeyword.cpp

--- Autokeyword.cpp
+++ Autokeyword.cpp
@@ -6,9 +6,27 @@ int main(){
 	for(int i=0;i<v.size();i++){
 		cout<<v[i]<<" ";
 	}cout<<endl;
-
+    
 	for(int value :v){
 		cout<<value<<" ";
 	}
+
+	for( int &value: v){
+		value++;
+	}cout<<endl;
+	for(int i=0;i<v.size();i++){
+
+
+		for(int &value :v){
+			cout<<value<<" ";
+			value++;
+		}cout<<endl;
+}
+
+vector<pair<int,int>> p={{1,2},{3,4},{5,6}};
+for(pair<int,int> value: p){        // Range function
+	cout<<value.first<<" "<<value.second<<" "<<endl;
+}
+
 
 }
