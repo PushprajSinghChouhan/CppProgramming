0b9d2fbbc291c45458dc264cd696a7581287b479
Pushpraj Singh Chouhan <113308159+PushprajSinghChouhan@users.noreply.github.com>
2024-08-06 17:07:38 +0530

    Add files via upload

--- Autokeyword.cpp
+++ Autokeyword.cpp
@@ -24,8 +24,13 @@ int main(){
 }
 
 vector<pair<int,int>> p={{1,2},{3,4},{5,6}};
-for(pair<int,int> value: p){        // Range function
+for(pair<int,int> value: p){        // Range based loops
 	cout<<value.first<<" "<<value.second<<" "<<endl;
+}
+
+vector<pair<int,int>> n={{11,12},{13,14},{15,16}};
+for(auto &value:  n){  // Auto
+	cout<<value.first<<" "<<value.second<<endl;
 }
 
 
