fad808c04e0aa396fc067101bdba5b2f09a7d9c3
Pushpraj Singh Chouhan <113308159+PushprajSinghChouhan@users.noreply.github.com>
2024-08-10 21:17:15 +0530

    Add files via upload

---
+++ set.cpp
@@ -0,0 +1,23 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+void Print(set<string>&s){
+	for(string value:s){
+		cout<<value<<endl;
+	}
+	for(auto it=s.begin();it!=s.end();++it){
+		cout<<(*it)<<endl;
+	}
+}
+
+int main(){
+	set<string>s;
+	s.insert("abc");
+	s.insert("a");
+	s.insert("ab");
+	s.insert("c");
+	s.insert("abcd");
+	auto it= s.find("abab");
+	
+	Print(s);
+}
